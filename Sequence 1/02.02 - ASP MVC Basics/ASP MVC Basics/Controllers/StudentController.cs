4efa3db7130c54a6cae8a4c561c9dd1524ef3240
Alexandre Nourissier <144102@supinfo.com>
2015-03-09 12:04:39 +0100

    correction(exo2): ASP MVC Basics

---
+++ Sequence 1/02.02 - ASP MVC Basics/ASP MVC Basics/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using ASP_MVC_Basics.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_MVC_Basics.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            var model = new Student(){
+                ID = 1,
+                Name ="Pierre Martin",
+                JoinDate = DateTime.Now,
+                Email = "pmartin@contoso.com",
+                HasPaid = false,
+                Class = 1
+            };
+            return View("Detail", "_Layout", model);
+        }
+
+    }
+}
---
+++ Sequence 1/02.02 - ASP MVC Basics/ASP MVC Basics/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_MVC_Basics.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Class { get; set; }
+        public DateTime JoinDate { get; set; }
+        public bool HasPaid { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        
+    }
+}
